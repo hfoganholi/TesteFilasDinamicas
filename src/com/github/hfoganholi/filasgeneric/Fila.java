5df2d17eaa7e1d9e5338dbf3659440e5b82d41f1
henrique <henrique@stockholm>
2026-04-04 11:12:14 -0300

    2026.04.04
    Atualização

--- src/com/github/hfoganholi/filasgeneric/Fila.java
+++ src/com/github/hfoganholi/filasgeneric/Fila.java
@@ -37,4 +37,40 @@ public class Fila {
 			}
 		}
 	}
+	
+	public int remove() throws Exception {
+		if (isEmpty()) {
+			throw new Exception ("Fila Vazia");
+		}
+		No auxiliar = inicio;
+		if (inicio == fim && inicio != null) {
+			inicio = null;
+			fim = null;
+		} else {
+			inicio = inicio.proximo;
+		}
+		return auxiliar.dado;
+	}
+	
+	public void list() throws Exception {
+		if (isEmpty()) {
+			throw new Exception("Fila Vazia");
+		}
+		No auxiliar = inicio;
+		while (auxiliar != null) {
+			auxiliar = auxiliar.proximo;
+		}
+	}
+	
+	public int size() {
+		int cont = 0;
+		if (!isEmpty()) {
+			No auxiliar = inicio;
+			while (auxiliar != null) {
+				cont++;
+				auxiliar = auxiliar.proximo;
+			}
+		}
+		return cont;
+	}
 }
